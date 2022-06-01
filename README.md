ad9f37fe5124e5c0ad7aaf0b100f8f2e193c348a
carthworks <tkarthikeyan@gmail.com>
2022-06-01 14:44:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-kqd6ff
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-kqd6ff)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Air Quality Open Data Platform</h1>
+      <p>https://api.waqi.info/feed/here/?token=faa35555d0b7db6950e3f2135abe61b424b6b015</p>
+    </div>
+  );
+}
